220683db644d57894ecb42c405d0aff506a517b5
Jerry Needell <jerryneedell@gmail.com>
2019-01-20 10:48:15 -0500

    creat homework#1 file

---
+++ Homework_1/song.js
@@ -0,0 +1,56 @@
+
+// Come Home Pretty Bird -- Emily Smith 
+
+var artist = {
+firstName: "Emily",
+lastName: "Smith",
+yearBorn: 1981,
+};
+
+var songTitle = "Come Home Pretty Bird";
+var durationInSeconds = 198;
+var genre = "Folk";
+var author = {
+firstName: "Emily",
+lastName: "Smith",
+};
+var albumTitle = "Too Long Away";
+var songYear = 2008;
+var albumYear = 2008;
+var recordLabel = "White Fall Records";
+var otherAttributes = [ "Scottish", "Female vocalist"];
+var instrument = [ "voice", "guitar"];
+var solo = true;
+
+
+var albumSongs = {
+1:"Sunset Hymn",
+2:"Caledonia",
+3:"Audience of Souls",
+4:"As I was a Wand’ring",
+5:"May Colven",
+6:"Mermaid of Galloway",
+7:"The Bleacher Lassie o’ Kelvinhough",
+8:"Come Home Pretty Bird",
+9:"Old Mortality",
+10:"Winter Song" 
+};
+
+
+console.log(artist);
+console.log(genre);
+console.log(songTitle);
+console.log(durationInSeconds);
+console.log(genre);
+console.log(author);
+console.log(albumTitle);
+console.log(songYear);
+console.log(albumYear);
+console.log(recordLabel);
+console.log(otherAttributes);
+console.log(instrument);
+console.log(solo);
+console.log(albumSongs);
+//console.log(lyrics);
+
+
